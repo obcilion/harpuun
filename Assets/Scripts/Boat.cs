dd5823beb5e0e83e8160eebda16f761a5f41b33e
obcilion <raymond.gulbrandsen@shortcut.no>
2017-01-02 21:13:54 +0100

    Added Scene & Prefabs. Added ui for controlling boat

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boat : MonoBehaviour
 {
+	public GameObject ThrottleSlider;
+	private Slider throttle_slider;
+
+	public GameObject RudderSlider;
+	private Slider rudder_slider;
+
+	[SerializeField]
+	private float turn_multiplier = 1f;
+	public float TurnMultiplier
+	{
+		get { return turn_multiplier; }
+		set { turn_multiplier = value; }	
+	}
+
 	[SerializeField]
 	private float speed_multiplier = 1f;
 	public float SpeedMultiplier
@@ -21,6 +36,7 @@ public class Boat : MonoBehaviour
 	}
 
 	[SerializeField]
+	[Range(-1,2)]
 	private int throttle_level = 0;
 	public int ThrottleLevel
 	{
@@ -37,6 +53,7 @@ public class Boat : MonoBehaviour
 	}
 
 	[SerializeField]
+	[Range(-180f,180f)]
 	private float rudder_angle = 0f;
 	public float RudderAngle
 	{
@@ -44,17 +61,28 @@ public class Boat : MonoBehaviour
 		set { rudder_angle = value; }
 	}
 
+	public void UpdateThrottle()
+	{
+		ThrottleLevel = (int)throttle_slider.value;
+	}
+
+	public void UpdateRudder()
+	{
+		RudderAngle = rudder_slider.value;
+	}
+
 	// Use this for initialization
 	void Start() 
 	{
-		
+		throttle_slider = ThrottleSlider.GetComponent<Slider> ();
+		rudder_slider = RudderSlider.GetComponent<Slider> ();
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		transform.Rotate (Vector3.up * Time.deltaTime * RudderAngle);
-		Speed = Mathf.MoveTowards (Speed, ThrottleLevel, acceleration);
+		transform.Rotate (Vector3.up * Time.deltaTime * RudderAngle * TurnMultiplier);
+		Speed = Mathf.MoveTowards (Speed, ThrottleLevel, Acceleration);
 		transform.Translate(Vector3.forward * Time.deltaTime * speed * speed_multiplier);
 	}
 }
