50c4df8b219b937c06d71594cab7fbc6931dafa1
obcilion <raymond.gulbrandsen@shortcut.no>
2017-01-03 19:39:55 +0100

    Added audio and pitch change to boat

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -5,84 +5,107 @@ using UnityEngine.UI;
 
 public class Boat : MonoBehaviour
 {
-	public GameObject ThrottleSlider;
-	private Slider throttle_slider;
+	#region Fields and Constants
 
-	public GameObject RudderSlider;
+	private Slider throttle_slider;
 	private Slider rudder_slider;
+	private AudioSource engine_audio;
+
+	private Dictionary<int, float> throttle_pitch_mapping = new Dictionary<int, float> () {
+		{ -1, 1f },
+		{ 0, 0.9f },
+		{ 1, 1f },
+		{ 2, 1.1f }
+	};
+
+	#endregion
+
+	#region Properties
 
 	[SerializeField]
 	private float turn_multiplier = 1f;
-	public float TurnMultiplier
-	{
+
+	public float TurnMultiplier {
 		get { return turn_multiplier; }
 		set { turn_multiplier = value; }	
 	}
 
 	[SerializeField]
 	private float speed_multiplier = 1f;
-	public float SpeedMultiplier
-	{
+
+	public float SpeedMultiplier {
 		get { return speed_multiplier; }
 		set { speed_multiplier = value; }	
 	}
 
 	[SerializeField]
 	private float acceleration = 1f;
-	public float Acceleration
-	{
+
+	public float Acceleration {
 		get { return acceleration; }
 		set { acceleration = value; }
 	}
 
 	[SerializeField]
-	[Range(-1,2)]
 	private int throttle_level = 0;
-	public int ThrottleLevel
-	{
+
+	public int ThrottleLevel {
 		get { return throttle_level; }
 		set { throttle_level = value; }
 	}
 
 	[SerializeField]
 	private float speed = 0f;
-	public float Speed
-	{
+
+	public float Speed {
 		get { return speed; }
 		private set { speed = value; }
 	}
 
 	[SerializeField]
-	[Range(-180f,180f)]
+	[Range (-180f, 180f)]
 	private float rudder_angle = 0f;
-	public float RudderAngle
-	{
+
+	public float RudderAngle {
 		get { return rudder_angle; }
 		set { rudder_angle = value; }
 	}
 
-	public void UpdateThrottle()
+	#endregion
+
+	#region Public Methods
+
+	public void UpdateThrottle ()
 	{
 		ThrottleLevel = (int)throttle_slider.value;
+		engine_audio.pitch = throttle_pitch_mapping [ThrottleLevel];
 	}
 
-	public void UpdateRudder()
+	public void UpdateRudder ()
 	{
 		RudderAngle = rudder_slider.value;
 	}
 
-	// Use this for initialization
-	void Start() 
+	#endregion
+
+	#region Unity Methods
+
+	void Start ()
 	{
-		throttle_slider = ThrottleSlider.GetComponent<Slider> ();
-		rudder_slider = RudderSlider.GetComponent<Slider> ();
+		throttle_slider = GameObject.FindGameObjectWithTag ("Throttle Slider").GetComponent<Slider> ();
+		rudder_slider = GameObject.FindGameObjectWithTag ("Rudder Slider").GetComponent<Slider> ();
+		engine_audio = GameObject.FindGameObjectWithTag ("Engine Audio").GetComponent<AudioSource> ();
+
+		UpdateThrottle ();
+		UpdateRudder ();
 	}
-	
-	// Update is called once per frame
-	void Update()
+
+	void Update ()
 	{
 		transform.Rotate (Vector3.up * Time.deltaTime * RudderAngle * TurnMultiplier);
 		Speed = Mathf.MoveTowards (Speed, ThrottleLevel, Acceleration);
-		transform.Translate(Vector3.forward * Time.deltaTime * speed * speed_multiplier);
+		transform.Translate (Vector3.forward * Time.deltaTime * speed * speed_multiplier);
 	}
+
+	#endregion
 }
